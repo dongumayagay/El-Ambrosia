ee38ab5004153a4176f9339c3654bf25ba61d91f
Don Gumayagay <dngumayagay@gmail.com>
2022-09-11 00:46:09 +0800

    added static files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
+	import { onMount } from 'svelte';
+
 	import '../app.postcss';
+
+	let loading = true;
+	onMount(() => {
+		loading = false;
+	});
 </script>
 
-<slot />
+<div class=" h-full bg-black text-white">
+	{#if loading}
+		<div class="h-full flex flex-col justify-center items-center">
+			<span class="text-orange-300 text-lg font-mestizo"> EL AMBROSIA </span>
+		</div>
+	{:else}
+		<slot />
+		<nav>test</nav>
+	{/if}
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,32 @@
+// const typography = require('@tailwindcss/typography');
 const forms = require('@tailwindcss/forms');
 
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		screens: {
+			sm: '640px',
+
+			md: '768px',
+
+			lg: '1024px',
+
+			xl: '1280px'
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '2rem',
+				sm: '0'
+			}
+		},
+		extend: {
+			fontFamily: {
+				anton: 'anton',
+				mestizo: 'mestizo'
+			}
+		}
 	},
 
 	plugins: [forms]
