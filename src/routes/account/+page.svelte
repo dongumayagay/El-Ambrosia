8bba92d7a1c9a49917deec4603cc953f9a964ffb
Don Gumayagay <dngumayagay@gmail.com>
2022-09-11 23:22:55 +0800

    prototype mockup

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,3 +1,12 @@
-<script>
-	//
-</script>
+<main class="flex-1 overflow-auto text-center px-4 flex flex-col gap-4 justify-center items-center">
+	<!--  -->
+	<button class="border max-w-xs w-full py-2 bg-red-900 font-anton tracking-wider">
+		Sign in with Email
+	</button>
+	<button class="border max-w-xs w-full py-2 bg-red-900 font-anton tracking-wider">
+		Sign in with Google
+	</button>
+	<button class="border max-w-xs w-full py-2 bg-red-900 font-anton tracking-wider"
+		>Sign in with Facebook</button
+	>
+</main>
