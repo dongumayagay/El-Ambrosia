d1b076ed3c1bd5c73e6cff6144938beed363411c
Don Gumayagay <dngumayagay@gmail.com>
2022-09-22 23:09:55 +0800

    about section added

---
+++ src/lib/components/AboutSection.svelte
@@ -0,0 +1,20 @@
+<div id="About" class=" mx-auto max-w-7xl py-12 px-4 flex flex-col items-center gap-6 md:flex-row">
+	<img
+		src="https://via.placeholder.com/400.webp/?text=About%20El%20Ambrosia"
+		alt="about el ambrosia"
+	/>
+	<article class="flex flex-col gap-2">
+		<h1 class="text-center text-xl uppercase">About El Ambrosia</h1>
+		<p class="text-center">
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo laborum possimus facere
+			pariatur, explicabo ad blanditiis autem, molestiae velit, nostrum numquam suscipit ab
+			molestias sunt vero ipsum maxime nobis ipsa atque. Harum, nihil distinctio aspernatur rerum
+			recusandae ipsa iure in quod cupiditate vel suscipit magni architecto! Illum aliquam
+			asperiores, pariatur sapiente harum laboriosam tenetur perspiciatis quia sequi vel rerum
+			repudiandae earum iure, voluptatem quas quae, accusantium maxime consectetur fugit
+			praesentium? Dicta necessitatibus dolorum exercitationem ut quam vel quia nesciunt distinctio
+			aperiam rem excepturi minima corrupti, at harum est maiores earum itaque natus. Nisi beatae et
+			necessitatibus sit quaerat nesciunt hic!
+		</p>
+	</article>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,30 @@
 <script lang="ts">
+	import AboutSection from '$lib/components/AboutSection.svelte';
 	import Carousel from '$lib/components/carousel/Carousel.svelte';
 	import type { CarouselItemInterface } from '$lib/types';
 
 	const carouselItems: CarouselItemInterface[] = [
 		{
 			name: '0',
-			image: 'https://via.placeholder.com/300.webp/?text=El%20Ambrosia%200',
+			image: 'https://via.placeholder.com/1000.webp/?text=El%20Ambrosia%200',
 			link: '/'
 		},
 		{
 			name: '1',
-			image: 'https://via.placeholder.com/300.webp/?text=El%20Ambrosia%201',
+			image: 'https://via.placeholder.com/1000.webp/?text=El%20Ambrosia%201',
 			link: '/'
 		},
 		{
 			name: '2',
-			image: 'https://via.placeholder.com/300.webp/?text=El%20Ambrosia%202',
+			image: 'https://via.placeholder.com/1000.webp/?text=El%20Ambrosia%202',
 			link: '/'
 		}
 	];
 </script>
 
-<div class=" h-96">
+<svelte:head><title>Home | El Ambrosia</title></svelte:head>
+
+<div id="Products" class="h-[75%]">
 	<Carousel {carouselItems} />
 </div>
+<AboutSection />
