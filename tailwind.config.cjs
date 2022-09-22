34dc358bbb29e91c4b67a8493bf6c23ea2ecb3c1
Don Gumayagay <dngumayagay@gmail.com>
2022-09-22 20:19:16 +0800

    change tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,22 +6,6 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		screens: {
-			sm: '640px',
-
-			md: '768px',
-
-			lg: '1024px',
-
-			xl: '1280px'
-		},
-		container: {
-			center: true,
-			padding: {
-				DEFAULT: '2rem',
-				sm: '0'
-			}
-		},
 		extend: {
 			fontFamily: {
 				anton: 'anton',
