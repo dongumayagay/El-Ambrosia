4049a2bd1f25d3a18c3a79f94682f67d5a7c3a12
Don Gumayagay <dngumayagay@gmail.com>
2022-09-24 10:19:46 +0800

    organize navbar items

---
+++ src/lib/components/navbar/AccountButton.svelte
@@ -0,0 +1,33 @@
+<div class="dropdown dropdown-end">
+	<button tabindex="0" class="btn btn-ghost btn-circle">
+		<div class="indicator">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+				/>
+			</svg>
+		</div>
+	</button>
+	<ul
+		tabindex="0"
+		class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+	>
+		<li>
+			<a href="/" class="justify-between">
+				Profile
+				<!-- <span class="badge">New</span> -->
+			</a>
+		</li>
+		<li><a href="/">Settings</a></li>
+		<li><a href="/">Logout</a></li>
+	</ul>
+</div>
---
+++ src/lib/components/navbar/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+	import HomeLogoButton from './HomeLogoButton.svelte';
+	import CartButton from './CartButton.svelte';
+	import AccountButton from './AccountButton.svelte';
+	import NotificationButton from './NotificationButton.svelte';
+</script>
+
+<nav class="fixed z-10 w-full bg-base-100 top-0">
+	<div class="navbar mx-auto max-w-7xl ">
+		<HomeLogoButton />
+		<div class="flex-none">
+			<CartButton />
+			<NotificationButton />
+			<AccountButton />
+		</div>
+	</div>
+</nav>
