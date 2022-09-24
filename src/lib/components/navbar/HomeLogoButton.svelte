5228c539430bbf1bdcd5d06dffa3d4d474c07f1d
Don Gumayagay <dngumayagay@gmail.com>
2022-09-24 11:12:54 +0800

    update pwa assets

--- src/lib/components/navbar/HomeLogoButton.svelte
+++ src/lib/components/navbar/HomeLogoButton.svelte
@@ -1,5 +1,8 @@
 <div class="flex-1">
-	<a href="/" class="btn btn-ghost normal-case text-xl flex gap-1">
+	<a
+		href="/"
+		class="btn btn-ghost normal-case flex items-center justify-center gap-1 text-xl tracking-wide"
+	>
 		<span class=""> El </span>
 		<span class=" text-primary"> Ambrosia </span>
 	</a>
