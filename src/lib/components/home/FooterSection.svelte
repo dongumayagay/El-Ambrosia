b2b42c7f1b427a0c0757c4463d5880ed07b651ee
Don Gumayagay <dngumayagay@gmail.com>
2022-09-24 10:50:41 +0800

    added favicon

--- src/lib/components/home/FooterSection.svelte
+++ src/lib/components/home/FooterSection.svelte
@@ -1,18 +1,17 @@
 <main class="footer max-w-7xl mx-auto p-10 text-base-content">
 	<div>
+		<span class="footer-title">Company</span>
+		<a href="#About" class="link link-hover">About us</a>
+		<a href="/" class="link link-hover">Contact</a>
+	</div>
+	<div>
 		<span class="footer-title">Services</span>
 		<a href="/" class="link link-hover">Branding</a>
 		<a href="/" class="link link-hover">Design</a>
 		<a href="/" class="link link-hover">Marketing</a>
 		<a href="/" class="link link-hover">Advertisement</a>
 	</div>
-	<div>
-		<span class="footer-title">Company</span>
-		<a href="/" class="link link-hover">About us</a>
-		<a href="/" class="link link-hover">Contact</a>
-		<a href="/" class="link link-hover">Jobs</a>
-		<a href="/" class="link link-hover">Press kit</a>
-	</div>
+
 	<div>
 		<span class="footer-title">Legal</span>
 		<a href="/" class="link link-hover">Terms of use</a>
--- src/lib/components/navbar/HomeLogoButton.svelte
+++ src/lib/components/navbar/HomeLogoButton.svelte
@@ -1,6 +1,6 @@
 <div class="flex-1">
 	<a href="/" class="btn btn-ghost normal-case text-xl flex gap-1">
-		<span> El </span>
+		<span class=""> El </span>
 		<span class=" text-primary"> Ambrosia </span>
 	</a>
 </div>
