1e576c659ca282294b363eb85a2d8747bb672910
Don Gumayagay <dngumayagay@gmail.com>
2022-10-15 13:42:27 +0800

    update all image

--- src/lib/components/ratings/RatingCard.svelte
+++ src/lib/components/ratings/RatingCard.svelte
@@ -1,9 +1,5 @@
 <article class="card lg:card-side bg-base-100 shadow-xl grid sm:grid-cols-2">
-	<img
-		src="https://scontent.fmnl25-5.fna.fbcdn.net/v/t39.30808-6/264268991_133568989062737_7382297756764015564_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0debeb&_nc_ohc=yUkZdd7npUYAX-K3ugu&tn=HgE7mpWVjK-Uz88Y&_nc_ht=scontent.fmnl25-5.fna&oh=00_AT_tXmWfnvsPJ9Dmljz7sGzjyg75eIy8j11vETlg-43NzA&oe=6333D83F"
-		alt="about el ambrosia"
-		class="w-full max-h-96 md:h-full object-cover aspect-square"
-	/>
+	<img src="/images/review.jpg" class="w-full max-h-96 md:h-full object-cover aspect-square" />
 	<div class="card-body">
 		<div class="rating gap-2 mx-auto">
 			<input disabled type="radio" name="rating-3" class="mask mask-heart bg-red-400" />
--- src/routes/api/products/+server.ts
+++ src/routes/api/products/+server.ts
@@ -6,17 +6,17 @@ export const GET: RequestHandler = async () => {
     const carouselItems: CarouselItemInterface[] = [
         {
             name: '0',
-            image: 'https://scontent.fmnl25-5.fna.fbcdn.net/v/t39.30808-6/243705065_117847190634917_653339844597567716_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=fPaxh7gUe4sAX-KvOdU&_nc_ht=scontent.fmnl25-5.fna&oh=00_AT-dZVO9DglatUnc1fk07sQ0-wlnXWkc0yIOvwBVrXyjvw&oe=63343F6E',
+            image: '/images/1.jpg',
             link: '/'
         },
         {
             name: '1',
-            image: 'https://scontent.fmnl25-5.fna.fbcdn.net/v/t39.30808-6/243519876_117847130634923_4447439677517139666_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=UisRNgMHRV0AX-aw-fG&_nc_ht=scontent.fmnl25-5.fna&oh=00_AT94swokY4T0Ys0PK-UVGzkXmsAftfsH839WMWiEA0m3_g&oe=6334B7E2',
+            image: '/images/2.jpg',
             link: '/'
         },
         {
             name: '2',
-            image: 'https://scontent.fmnl25-1.fna.fbcdn.net/v/t39.30808-6/244367001_117846543968315_5007865927415458510_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=_bMqu8e1k4kAX9JP0kt&_nc_ht=scontent.fmnl25-1.fna&oh=00_AT8jSCxvTgO-i-QOL8qnVJ3jfG76l2Ynnu3lawFUyscIPQ&oe=63339AD5',
+            image: '/images/3.jpg',
             link: '/'
         }
     ];
