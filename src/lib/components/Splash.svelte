8c42e93d887c9fc608a240d9f1b668aa4328a0fb
Don Gumayagay <dngumayagay@gmail.com>
2022-11-09 07:21:01 +0800

    added splash screen

---
+++ src/lib/components/Splash.svelte
@@ -0,0 +1,8 @@
+<main class="relative h-screen bg-black ">
+	<img
+		src="/favicon.png"
+		alt="El Amborosia logo"
+		class="absolute h-24 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2"
+	/>
+	<h1 class="absolute text-2xl text-white -translate-x-1/2 left-1/2 bottom-12">El Ambrosia</h1>
+</main>
