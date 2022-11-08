be0d6d4c0186407448c64a9d170dd485deaf3806
Don Gumayagay <dngumayagay@gmail.com>
2022-11-09 06:15:13 +0800

    remove supabase

--- src/lib/supabase/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-// @ts-ignore
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from "$env/static/public"
-
-export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
--- src/routes/api/products/+server.ts
+++ src/routes/api/products/+server.ts
@@ -20,26 +20,5 @@ export const GET: RequestHandler = async () => {
             link: '/'
         }
     ];
-
-
     return new Response(JSON.stringify(carouselItems));
 };
-
-// import { supabase } from '$lib/supabase/client';
-// import { error } from '@sveltejs/kit';
-
-// export async function load({}) {
-//     const {data:products,error:err} = await supabase
-//         .from('products')
-//         .select(
-//             `id,name,description,image,type,
-//             product_variants(
-//             id,name,price
-//             )`
-//         )
-//     if (err) throw error(404, 'Not found');
-//     return{
-//         products
-//     }
-// }
-
