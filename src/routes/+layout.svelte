fb985f8e7e14492e6ebc1aa62ece21b44dff0368
Don Gumayagay <dngumayagay@gmail.com>
2022-11-09 08:42:27 +0800

    add google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+	import { app } from '$lib/firebase';
+	import { getAnalytics } from 'firebase/analytics';
+	import { onMount } from 'svelte';
 	import '../app.postcss';
+	
+	onMount(() => {
+		getAnalytics(app);
+	});
 </script>
 
 <svelte:head><title>El Ambrosia</title></svelte:head>
-<main class="h-full min-h-full relative bg-base-300 ">
+<main class="relative h-full min-h-full bg-base-300 ">
 	<slot />
 </main>
