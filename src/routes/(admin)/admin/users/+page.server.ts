fd421425c6a22087a1db5de5f7c75ed769b90ac2
Don Gumayagay <dngumayagay@gmail.com>
2022-11-09 17:25:25 +0800

    added page server for loading students

---
+++ src/routes/(admin)/admin/users/+page.server.ts
@@ -0,0 +1,16 @@
+import { adminAuth } from "$lib/firebase/admin"
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ depends }) => {
+    const result = await adminAuth.listUsers()
+    console.log('admin users load function run')
+    const dependency = 'admin:users'
+    depends(dependency)
+    if (result) {
+        const users = result.users.map(user => user.toJSON())
+        return { users, dependency };
+    }
+
+    throw error(500, 'Loading users failed');
+}   
