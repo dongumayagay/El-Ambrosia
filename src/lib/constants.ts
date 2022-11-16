d3ece3fec611e9eb81f627df7da9b17dd229e11c
Don Gumayagay <dngumayagay@gmail.com>
2022-11-16 15:26:09 +0800

    added menu price matrix constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,36 @@
+export const SHAWARMAS = [
+    {
+        name: 'underdog 6"',
+        description: '6" inch Kuboos bread, wrap with Classic fries, pickles & El Ambrosia Signature Garlic Sauce.',
+        variants: {
+            'chicken': 69,
+            'steak': 85
+        },
+
+    },
+    {
+        name: 'big honcho 8"',
+        description: '8" inch Kuboos bread, wrap with Classic fries, pickles & El Ambrosia Signature Garlic Sauce.',
+        variants: {
+            'chicken': 95,
+            'steak': 109
+        },
+
+    },
+    {
+        name: 'bossman 12"',
+        description: '12" inch Kuboos bread, wrap with Classic fries, pickles & El Ambrosia Signature Garlic Sauce.',
+        variants: {
+            'chicken': 175,
+            'steak': 189
+        },
+
+    },
+]
+export let EXTRAS = {
+    'chicken': 25,
+    'steak': 35,
+    'classic fries medium': 29,
+    'classic fries xtra large': 55,
+    'garclic sauce': 20
+}
