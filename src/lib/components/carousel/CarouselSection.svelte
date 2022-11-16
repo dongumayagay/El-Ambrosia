7a1226463eb98c7eeae6e6fba01c150c209bdb25
Don Gumayagay <dngumayagay@gmail.com>
2022-11-16 15:29:41 +0800

    simplify carousel

--- src/lib/components/carousel/CarouselSection.svelte
+++ src/lib/components/carousel/CarouselSection.svelte
@@ -2,7 +2,20 @@
 	import Carousel from './Carousel.svelte';
 	import type { CarouselItemInterface } from '$lib/types';
 
-	export let carouselItems: CarouselItemInterface[];
+	const carouselItems: CarouselItemInterface[] = [
+		{
+			name: '0',
+			image: '/images/1.jpg'
+		},
+		{
+			name: '1',
+			image: '/images/2.jpg'
+		},
+		{
+			name: '2',
+			image: '/images/3.jpg'
+		}
+	];
 </script>
 
 <section id="Products" class=" h-5/6">
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import Carousel from '$lib/components/carousel/Carousel.svelte';
+	import CarouselSection from '$lib/components/carousel/CarouselSection.svelte';
+	import MenuSection from '$lib/components/menu/MenuSection.svelte';
 	import AboutSection from './AboutSection.svelte';
 	import RatingsSection from '$lib/components/ratings/RatingsSection.svelte';
-	import type { CarouselItemInterface } from '$lib/types';
-	import MenuSection from '$lib/components/menu/MenuSection.svelte';
-	import CarouselSection from '$lib/components/carousel/CarouselSection.svelte';
-
-	export let data: PageData;
-	const carouselItems: CarouselItemInterface[] = data.carouselItems;
 </script>
 
-<CarouselSection {carouselItems} />
+<CarouselSection />
 <MenuSection />
 <AboutSection />
 <RatingsSection />
