8f4d3a3f398683822ee89fd39363714e6b3f6b60
Don Gumayagay <dngumayagay@gmail.com>
2022-11-19 05:47:40 +0800

    added firebase auth and auth listener

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,3 +1,18 @@
-import { writable } from "svelte/store";
+import { readable } from "svelte/store";
+import { onAuthStateChanged, type Auth, type User } from "firebase/auth"
+import { auth } from "./firebase/client";
 
-export const userStore = writable()
+const createAuthStore = (auth: Auth) => readable<User | null | undefined>(
+    undefined,
+    set => {
+        const unsubscribe = onAuthStateChanged(
+            auth,
+            (user) => {
+                set(user)
+            }
+        )
+        return () => unsubscribe()
+    }
+)
+
+export const userStore = createAuthStore(auth)
