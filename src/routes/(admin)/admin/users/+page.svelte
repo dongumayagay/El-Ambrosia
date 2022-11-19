6c7247c8921151b638d31352dcd5c851a09d5a33
Don Gumayagay <dngumayagay@gmail.com>
2022-11-19 09:32:47 +0800

    improve refresh on users page

--- src/routes/(admin)/admin/users/+page.svelte
+++ src/routes/(admin)/admin/users/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import type { UserRecord } from 'firebase-admin/auth';
+	import Refresh from './Refresh.svelte';
+
 	const loadUsers = async () => (users = await (await fetch('/api/users')).json());
+
 	let users: UserRecord[];
 	loadUsers();
 </script>
@@ -23,23 +26,7 @@
 		</svg>
 		Create User
 	</a>
-	<button class="gap-2 btn btn-primary" on:click={loadUsers}
-		><svg
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke-width="1.5"
-			stroke="currentColor"
-			class="w-6 h-6"
-		>
-			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
-			/>
-		</svg>
-		refresh</button
-	>
+	<Refresh {loadUsers} />
 </header>
 <div class="w-full overflow-x-auto">
 	<table class="table w-full">
@@ -58,7 +45,7 @@
 					<td colspan="5">
 						<h1 class="flex items-center justify-center">
 							<svg
-								class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+								class="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
 								viewBox="0 0 24 24"
--- src/routes/(admin)/admin/users/[uid]/DeleteUser.svelte
+++ src/routes/(admin)/admin/users/[uid]/DeleteUser.svelte
@@ -55,7 +55,7 @@
 				<button type="submit" class="btn btn-error" disabled={loading}>
 					{#if loading}
 						<svg
-							class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+							class="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
 							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
 							viewBox="0 0 24 24"
