116c21c0da213b993d0b2daa16b949aaa374776a
Don Gumayagay <dngumayagay@gmail.com>
2022-11-23 17:10:29 +0800

    fix import error

--- src/routes/(admin)/admin/support/+page.svelte
+++ src/routes/(admin)/admin/support/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import SearchBar from '$lib/components/admin/SearchBar.svelte';
-	import PaginationButton from '$lib/components/admin/PaginationButton.svelte';
+	// import SearchBar from '$lib/components/admin/SearchBar.svelte';
+	// import PaginationButton from '$lib/components/admin/PaginationButton.svelte';
 	import ChatPreviewCard from './ChatPreviewCard.svelte';
 </script>
 
 <div class="flex flex-col justify-start w-full gap-2 p-2 mx-auto max-w-7xl sm:flex-row-reverse">
-	<SearchBar searchItem={'support'} />
+	<!-- <SearchBar searchItem={'support'} /> -->
 </div>
 <main class="overflow-auto">
 	{#each { length: 20 } as _}
@@ -13,4 +13,4 @@
 	{/each}
 </main>
 
-<PaginationButton />
+<!-- <PaginationButton /> -->
