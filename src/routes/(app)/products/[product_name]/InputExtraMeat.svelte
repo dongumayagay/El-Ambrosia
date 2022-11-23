eccf9ea0fca16e41cd31b6e14ac182a1d3ea5237
Don Gumayagay <dngumayagay@gmail.com>
2022-11-23 17:26:27 +0800

    minor ui changes

--- src/routes/(app)/products/[product_name]/InputExtraMeat.svelte
+++ src/routes/(app)/products/[product_name]/InputExtraMeat.svelte
@@ -7,13 +7,13 @@
 </script>
 
 <h1 class="text-xl font-bold">Extra {meat?.name ?? 'Meat'}?</h1>
-<div class=" gap-4 grid grid-cols-2">
-	<label class="btn btn-outline justify-between" class:btn-active={extra_meat === false}>
+<div class="grid grid-cols-2 gap-2 ">
+	<label class="justify-between btn btn-outline" class:btn-active={extra_meat === false}>
 		<h1>Nah</h1>
 		<h1>₱ 0</h1>
 		<input class="hidden" type="radio" bind:group={extra_meat} value={false} required />
 	</label>
-	<label class="btn btn-outline justify-between" class:btn-active={extra_meat === true}>
+	<label class="justify-between btn btn-outline" class:btn-active={extra_meat === true}>
 		<h1>Yes!</h1>
 		<h1>
 			₱ {EXTRA_MEAT[meat?.name] ?? '0'}
--- src/routes/(app)/products/[product_name]/InputFries.svelte
+++ src/routes/(app)/products/[product_name]/InputFries.svelte
@@ -4,15 +4,15 @@
 	export let fries: Variant | undefined | null;
 </script>
 
-<h1 class="text-xl font-bold">Extra Garlic sauce?</h1>
-<div class="  grid grid-cols-1 gap-2 ">
-	<label class="btn btn-outline justify-between" class:btn-active={fries === null}>
+<h1 class="text-xl font-bold">Extra Fries?</h1>
+<div class="grid grid-cols-1 gap-2 ">
+	<label class="justify-between btn btn-outline" class:btn-active={fries === null}>
 		<h1>Nah</h1>
 		<h1>₱ 0</h1>
 		<input type="radio" class="hidden" bind:group={fries} value={null} required />
 	</label>
 	{#each FRIES as item}
-		<label class="btn btn-outline justify-between" class:btn-active={fries?.name === item.name}>
+		<label class="justify-between btn btn-outline" class:btn-active={fries?.name === item.name}>
 			<h1>{item.name}</h1>
 			<h1>₱ {item.price}</h1>
 			<input type="radio" class="hidden" bind:group={fries} value={item} required />
