429347b53e0e826db1c33b14c14baac44c97be67
Don Gumayagay <dngumayagay@gmail.com>
2022-11-23 10:28:48 +0800

    remove unused files and move component files

--- src/lib/components/admin/PaginationButton.svelte
+++
@@ -1,8 +0,0 @@
-<div class="grid items-center grid-cols-3 p-3 mt-auto btn-group">
-	<button class="btn btn-sm">Previous page</button>
-	<button class="btn btn-sm btn-disabled">
-		<span class="text-primary">20 of 100</span>
-	</button>
-
-	<button class="btn btn-sm">Next page</button>
-</div>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import CarouselSection from '$lib/components/carousel/CarouselSection.svelte';
-	import MenuSection from '$lib/components/menu/MenuSection.svelte';
+	import MenuSection from './MenuSection.svelte';
 	import AboutSection from './AboutSection.svelte';
 	import RatingsSection from '$lib/components/ratings/RatingsSection.svelte';
 </script>
