eea576c74ef514abc3a2d99bc8a3e7cbb8f6bce9
Don Gumayagay <dngumayagay@gmail.com>
2022-11-24 11:21:42 +0800

    transfer show cart to cart store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,7 +1,8 @@
-import { readable, writable } from "svelte/store";
+import { derived, readable, writable } from "svelte/store";
 import { onAuthStateChanged, type User } from "firebase/auth"
 import { auth } from "./firebase/client";
 import { browser } from "$app/environment";
+import type { CartItem } from "./types";
 
 export const userStore = readable<User | null | undefined>(undefined, set => {
     const unsubscribe = onAuthStateChanged(
@@ -11,4 +12,39 @@ export const userStore = readable<User | null | undefined>(undefined, set => {
     return () => unsubscribe()
 })
 
-export const showSideCart = writable<boolean>()
+
+export const cartStore = createCartStore()
+
+
+function createCartStore() {
+    const isSideCartOpen = writable<boolean>()
+    const {
+        set: setShowSideCart,
+        update: updateShowSideCart
+    } = isSideCartOpen
+    const toggleShowSideCart = (toggle?: boolean) => {
+        if (toggle === undefined) updateShowSideCart(value => !value)
+        else setShowSideCart(toggle)
+    }
+
+
+
+    const cartItems = writable<CartItem[]>([])
+    const { set: cartItemsSet, update: cartItemsUpdate } = cartItems
+    // const cartTotal = derived()
+    const addCartItem = (item: CartItem) =>
+        cartItemsUpdate((values) => ([...values, item]))
+    const clearCart = () => cartItemsSet([])
+    const removeCartItem = (item: CartItem) => cartItemsUpdate((values) => values.filter((value => value.name !== item.name)))
+
+
+
+    return {
+        isSideCartOpen,
+        toggleShowSideCart,
+        cartItems,
+        addCartItem,
+        removeCartItem,
+        clearCart,
+    }
+}
