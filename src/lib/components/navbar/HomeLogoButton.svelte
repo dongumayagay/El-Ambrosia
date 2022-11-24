4526dbd6ae66c1255e9aa60722b4c078a776ac01
Don Gumayagay <dngumayagay@gmail.com>
2022-11-24 11:22:04 +0800

    remove unused files

--- src/lib/components/navbar/HomeLogoButton.svelte
+++
@@ -1,8 +0,0 @@
-<div class="flex-1">
-	<a
-		href="/"
-		class="flex items-center justify-center text-xl normal-case btn btn-ghost font-anton tracking-widest "
-	>
-		<h1 class="">El Ambrosia</h1>
-	</a>
-</div>
