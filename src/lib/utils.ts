362ebdbdd605319543fdee78d74f642470cd4874
Don Gumayagay <dngumayagay@gmail.com>
2022-11-26 11:03:18 +0800

    add helper function to send email

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,3 +13,18 @@ export const isAdmin = (user: User | null | undefined) => {
     if (!admin) return false
     return true
 }
+
+
+export const sendEmail = async (
+    mailOptions = {
+        to: 'dngumayagay@gmail.com',
+        subject: 'Email Subject',
+        html: '<h1>Hello World</h1>'
+    }
+) =>
+    await (
+        await fetch('/api/send-email', {
+            method: 'POST',
+            body: JSON.stringify({ mailOptions })
+        })
+    ).json();
