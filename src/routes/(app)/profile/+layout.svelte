7e724901fbd8d3d5ed6f4722ce498c7b4b1ae902
Don Gumayagay <dngumayagay@gmail.com>
2022-12-03 07:21:22 +0800

    finish contact page

--- src/routes/(app)/profile/+layout.svelte
+++ src/routes/(app)/profile/+layout.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import { goto } from '$app/navigation';
 	import Splash from '$lib/components/Splash.svelte';
 	import { userStore } from '$lib/stores';
@@ -7,13 +7,14 @@
 		toast.error('you are not logged in');
 		goto('/');
 	}
+	let checked: boolean;
 </script>
 
 {#if $userStore === undefined || $userStore === null}
 	<Splash />
 {:else if $userStore}
 	<div class="drawer drawer-mobile">
-		<input id="profile-sidebar" type="checkbox" class="drawer-toggle" />
+		<input id="profile-sidebar" type="checkbox" class="drawer-toggle" bind:checked />
 		<div class="flex flex-col drawer-content bg-base-200">
 			<!-- Page content here -->
 			<slot />
@@ -22,8 +23,10 @@
 			<label for="profile-sidebar" class="drawer-overlay" />
 			<nav class="menu p-4 w-80 bg-base-100 text-base-content gap-4">
 				<!-- Sidebar content here -->
-				<a class="btn gap-2" href="/profile">Contact Information</a>
-				<a class="btn gap-2" href="/profile/address">Address</a>
+				<a on:click={() => (checked = false)} class="btn gap-2" href="/profile"
+					>Contact Information</a
+				>
+				<a on:click={() => (checked = false)} class="btn gap-2" href="/profile/address">Address</a>
 			</nav>
 		</div>
 	</div>
--- src/routes/(app)/profile/+page.svelte
+++ src/routes/(app)/profile/+page.svelte
@@ -1,12 +1,49 @@
 <script lang="ts">
-	let contactInfo = {
+	import { db } from '$lib/firebase/client';
+	import { userStore } from '$lib/stores';
+	import { doc, getDoc, setDoc } from 'firebase/firestore';
+	import toast from 'svelte-french-toast';
+
+	type ContactInfo = {
+		firstName: string;
+		lastName: string;
+		phoneNumber: string;
+	};
+
+	let contactInfo: ContactInfo = {
 		firstName: '',
 		lastName: '',
 		phoneNumber: ''
 	};
+	let loading = false;
+
+	const getUserProfile = async () => {
+		if (!$userStore) return;
+		const docSnapshot = await getDoc(doc(db, 'userProfiles', $userStore.uid));
+		if (!docSnapshot.exists()) return;
+		contactInfo = docSnapshot.data() as ContactInfo;
+	};
+	const submitHandler = async () => {
+		if (!$userStore) return;
+		loading = true;
+		try {
+			await setDoc(doc(db, 'userProfiles', $userStore.uid), contactInfo);
+			getUserProfile();
+			toast.success('saved');
+		} catch (error: any) {
+			if (error?.code)
+				toast.error(error.code.split('/')[1].replaceAll('-', ' '), {
+					className: 'uppercase'
+				});
+			else toast.error(error as string);
+		}
+		loading = false;
+	};
+
+	$: $userStore && getUserProfile();
 </script>
 
-<form class="grid gap-4 p-4">
+<form on:submit|preventDefault={submitHandler} class="grid gap-4 p-4">
 	<label class="form-control">
 		<h1>Enter your first name</h1>
 		<input
@@ -50,5 +87,22 @@
 			Invalid phone number
 		</p>
 	</label>
-	<button class=" btn btn-info" type="submit">Save</button>
+	<button class=" btn btn-info gap-2" disabled={loading} type="submit">
+		{#if loading}
+			<svg
+				class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+			>
+				<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+				<path
+					class="opacity-75"
+					fill="currentColor"
+					d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+				/>
+			</svg>
+		{/if}
+		Save</button
+	>
 </form>
