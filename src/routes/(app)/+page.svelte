596838526f1392490907d65cbadd83cfcefd1d76
Don Gumayagay <dngumayagay@gmail.com>
2022-12-19 11:44:01 +0800

    add video hero banner

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -4,9 +4,11 @@
 	import AboutSection from '$lib/components/home/AboutSection.svelte';
 	import RatingsSection from '$lib/components/ratings/RatingsSection.svelte';
 	import Footer from '$lib/components/navbar/Footer.svelte';
+	import HeroSection from '$lib/components/home/HeroSection.svelte';
 </script>
 
-<CarouselSection />
+<HeroSection />
+<!-- <CarouselSection /> -->
 <MenuSection />
 <AboutSection />
 <RatingsSection />
