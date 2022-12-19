9ce4ca763063eb276aa9d10999959654942dc654
Don Gumayagay <dngumayagay@gmail.com>
2022-12-19 11:49:54 +0800

    test design

--- src/lib/components/home/HeroSection.svelte
+++ src/lib/components/home/HeroSection.svelte
@@ -1,5 +1,3 @@
-<section class=" bg-pink-400">
-	<video src="/hero_video.mp4" autoplay muted loop>
-		<track kind="captions" />
-	</video>
-</section>
+<video src="/hero_video.mp4" autoplay muted loop>
+	<track kind="captions" />
+</video>
